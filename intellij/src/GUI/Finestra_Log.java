47d6e3b6c670ac391c8ee15e5c4ba57fd38ba0d9
AleAcquaFiji <82036768+AleAcquaFiji@users.noreply.github.com>
2021-04-06 17:34:59 +0200

    finestra log

---
+++ intellij/src/GUI/Finestra_Log.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.*;
+
+public class Finestra_Log {
+    JFrame frame = new JFrame();
+
+        frame.setSize(600,600);
+
+}
---
+++ intellij/src/User/User.java
@@ -0,0 +1,25 @@
+package User;
+
+public class User {
+
+    private String nome;
+    private boolean colore;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isColore() {
+        return colore;
+    }
+
+    public void setColore(boolean colore) {
+        this.colore = colore;
+    }
+
+
+}
